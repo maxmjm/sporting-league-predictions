e6d19c3081bb6de5b4d3219f50b7160386133441
Maxime Morard <maxmjm@icloud.com>
2025-07-18 11:01:19 +0800

    Basic layout for all sporting leagues

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Navigate to specified page when the button is clicked
+function navigateToPage(pageName) {
+  if (pageName === 'nba-regular-season') {
+    window.location.href = 'nba-regular-season.html';
+  }
+}
+
+// Show an alert indicating a page is still in development
+function showInDevelopment() {
+  alert('This page is still in development! Please check back later.');
+}
